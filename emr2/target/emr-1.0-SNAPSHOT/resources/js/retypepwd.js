97f90439d2c52181ce49e69b161f49554ea60777
SuperNyancat <balagtas.ag@gmail.com>
2015-07-27 10:54:10 +0800

    Revert "EMR v1"
    This reverts commit 27410e969234588f0e67f0bf0b2fd7e9abafc2fa.

--- emr2/target/emr-1.0-SNAPSHOT/resources/js/retypepwd.js
+++
@@ -1,7 +0,0 @@
-$('#txtConfirmPassword').on('keyup', function () {
-    var password = $("#txtNewPassword").val();
-    var confirmPassword = $("#txtConfirmPassword").val();
-
-    if (password != confirmPassword) $("#divCheckPassword").html("Passwords do not match!");
-	else if (password == confirmPassword) $("#divCheckPassword").html("");
-});
